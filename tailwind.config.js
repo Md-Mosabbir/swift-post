7a0bf6751431a55e5bcc97311482849def0f44aa
Md-Mosabbir <kmosabbir@gmail.com>
2023-12-06 08:02:29 +0600

    Create the structure for the app

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.ejs", // Adjust the path based on your project structure
+    "./public/**/*.js", // Include JavaScript files if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
